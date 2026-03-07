fede1371de083e821762f62a6491c313fbd780b4
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2026-03-08 01:48:43 +0600

    Update LAB3.java

--- Lab Assignment Solution/LAB3.java
+++ Lab Assignment Solution/LAB3.java
@@ -23,3 +23,48 @@ public class Toy {
         this.price = price;
     } 
 }
+
+// Task 02
+public class Cart {  
+    public int number = 0;
+    public int cartSize = 3;
+    public double[] itemPrices = new  double [cartSize];
+    public String[] itemNames = new  String [cartSize];
+    public int itemCount = 0;
+    public double discount = 0;
+
+    public void create_cart(int number){
+        this.number = number;
+    }
+
+    public void cartDetails(){
+        double totalPrice = 0.0;
+        System.out.println("Your cart(c"+ number +") : ");
+        for (int i = 0; i < itemCount; i++){
+            System.out.println(itemNames[i] + " - " + itemPrices[i]);
+            totalPrice += itemPrices[i];
+        }
+        System.out.println("Discount Applied: "+ discount +"%");
+        System.out.println("Total price: " + (totalPrice - (totalPrice * (discount / 100))));
+    }
+
+    public void addItem (String itemName, double itemPrice){
+        if (itemCount < cartSize){
+            System.out.println(itemName + " added to cart " + number + ".");
+            itemPrices[itemCount] = itemPrice;
+            itemNames[itemCount] = itemName;
+            System.out.println("You have "+ ++itemCount +" item(s) in your cart now.");
+        } else {
+            System.out.println("You already have "+ cartSize +" items on your cart");
+            itemCount = cartSize;
+        }
+    }
+
+    public void addItem (double itemPrice, String itemName){
+        addItem(itemName, itemPrice);
+    }
+
+    public void giveDiscount (double discount){
+        this.discount = discount;
+    }
+}
