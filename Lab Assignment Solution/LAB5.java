017c9b96a832d2703058eb6a671f07f4146af8b1
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2026-04-20 02:22:57 +0600

    Update LAB5.java

--- Lab Assignment Solution/LAB5.java
+++ Lab Assignment Solution/LAB5.java
@@ -39,3 +39,74 @@ public class Product {
         if (!found) System.out.println("Product not found");
     }
 }
+
+// Task 02
+public class Character {
+
+    public int health;
+    public String name;
+    public String type;
+    public int _id;
+
+    public static String strongest = "None";
+    public static int Kids = 0;
+    public static int Teens = 0;
+    public static int Adults = 0;
+    public static int totalHealth;
+    public static int totalCharacters;
+    public static int maxHealth;
+    public static int id = 1;
+
+    public Character(String name, String type, int health) {
+        this.name = name;
+        this.type = type;
+        this.health = health;
+
+        switch(type){
+            case "Kid": 
+                Kids++;
+                break;
+            case "Adult": 
+                Adults++; 
+                break;
+            case "Teen": 
+                Teens++; 
+                break;
+        }
+
+        _id = id++;
+        totalHealth += health;
+        totalCharacters = Kids + Adults + Teens;
+
+        if (health > maxHealth) {
+            maxHealth = health;
+            strongest = name;
+        }
+    }
+
+    public Character(String name, int health) {
+        this(name, "Teen", health);
+    }
+
+    public static void printStats() {
+        int avgHealth = totalCharacters <= 0 ? 0 : totalHealth / totalCharacters;
+        System.out.println("Total Characters: " + totalCharacters);
+        System.out.println("Kids: " + Kids);
+        System.out.println("Teens: " + Teens);
+        System.out.println("Adults: " + Adults);
+        if (avgHealth == 0) {
+            System.out.println("Average Health: 0");
+        } else {
+            System.out.println("Average Health: " + (double) avgHealth);
+        }
+        if (strongest.equals("None")) System.out.println("Strongest Character: " + strongest); 
+        else System.out.println("Strongest Character:\n" + strongest + " (Health " + maxHealth + ")");
+    }
+
+    public void printDetails() {
+        System.out.println("ID: " + _id + ", Name: " + name);
+        System.out.println("Group: " + type);
+        System.out.println("Health: " + health);
+    }
+}
+
