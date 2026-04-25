8a3eb4d62030a0e885b0609ab117528b69e02284
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2026-04-25 23:52:22 +0600

    Create LAB6.java

---
+++ Lab Assignment Solution/LAB6.java
@@ -0,0 +1,16 @@
+// Task 01
+public class Cow extends Animal{
+    public String name;
+
+    public Cow(){
+        System.out.println("The cow says hello!");
+    }
+  
+    public String getName(){
+        return name;
+    }
+  
+    public void updateSound(String sound){
+        super.sound = sound;
+    }
+}
