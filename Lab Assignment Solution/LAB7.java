8d4d824a1c61fa6cb423e64db2d962007b95dcb9
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2026-05-04 23:34:01 +0600

    Refactor eat methods and add Concert class

--- Lab Assignment Solution/LAB7.java
+++ Lab Assignment Solution/LAB7.java
@@ -5,9 +5,7 @@ public class Butterfly extends Caterpillar {
   }
 
   public void eat() {
-    energy += 5;
-    energy -= 5;
-    System.out.println("Butterfly lost energy due to nocturnal activity");
+   eat(1);
   }
 
   public void eat(int amount) {
@@ -27,9 +25,7 @@ public class Moth extends Caterpillar {
   }
 
   public void eat() {
-    energy += 2.5;
-    energy -= 2.5;
-    System.out.println("Moth lost energy due to nocturnal activity");
+    eat(1);
   }
 
   public void eat(int amount) {
@@ -40,5 +36,72 @@ public class Moth extends Caterpillar {
   
   public void transform() {
     System.out.println("Caterpillar transforms into Moth");
+  }
+}
+
+// Task 02
+public class Concert {
+
+  public String name,time,artistList = "";
+  public int artistCount, soldTickets;
+  public static int totalSoldTickets;
+
+  public Concert (String name, String time){
+    this.name = name;
+    this.time = time;
+  }
+
+  public void addArtist(String artistName) {
+    if (artistCount <= 5){
+      artistList += "-" + artistName + "\n";
+      artistCount++;
+    } else {
+      System.out.println("Maximum artist reached");
+    }
+  }
+
+  public void showDetails() {
+    System.out.println("Venue: " + name);
+    System.out.println("Showtime: " + time);
+    System.out.println("Artist: ");
+    System.out.print(artistList);
+    System.out.println("Tickets sold in this concert: " + soldTickets);
+  }
+
+  public void buyTicket(String zone, int amount){
+    int price = 0;
+    switch(zone){
+      case "A":
+        soldTickets += amount;
+        price = 500 * amount;
+        break;
+      case "B":
+        soldTickets += amount;
+        price = 1000 * amount;
+        break;
+    }
+    System.out.println("Total price: " + price);
+    totalSoldTickets += amount;
+  }
+  
+  public void buyTicket(String zone){
+    buyTicket(zone,1);
+  }
+
+  public static void showTotalTicketsSold(){
+    System.out.println("Total tickets sold (all venues): "+ totalSoldTickets);
+  }
+}
+
+public class VIPConcert extends Concert {
+  
+  public VIPConcert (String name, String time){
+    super(name,time);
+  }
+
+  public void buyTicket(){
+    System.out.println("Total price: " + 2000);
+    soldTickets++;
+    totalSoldTickets++;
   }
 }
